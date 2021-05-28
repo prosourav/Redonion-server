69c5a520ea23d67bff47e4d6b32f70e8a971f563
prosourav <sourav19961010@gmail.com>
2021-05-28 23:54:25 +0530

    home page done

--- index.js
+++ index.js
@@ -36,11 +36,33 @@ app.get('/dishes',(req,res)=>{
                 }else{
                     res.send(documents);
                 }
-                client.close();
+                // client.close();
             })
         }
     })
 })
+
+app.get('/About',(req,res)=>{
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        if(err){
+            console.log(err);
+        }else{
+      
+            const collection =  client.db('redonion').collection('About');
+            collection.find().toArray((rej,documents) => {
+                if(rej){
+                    console.log(rej);
+                    res.status(500).send("Filed to Fetch Data ")
+                }else{
+                    res.send(documents);
+                }
+                // client.close();
+            })
+        }
+    })
+})
+
 app.post('/adddish',(req,res)=>{
     const Newfood = req.body;
     console.log(Newfood);
@@ -56,6 +78,24 @@ app.post('/adddish',(req,res)=>{
 
         })
     })
+})
+
+app.post('/addAbout',(req,res)=>{
+    const About = req.body;
+    console.log(About);
+    client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
+    client.connect(err=>{
+        const collection = client.db('redonion').collection('About');
+        collection.insertOne(About, (rej, result) =>  {
+        if(rej){
+            res.status(500).send("Filed to insert")
+        }else{
+            res.send(result.insertedId);
+        }
+
+        })
+    }
+    )
 })
 
 app.listen(port, () => {
