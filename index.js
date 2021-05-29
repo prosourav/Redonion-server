f6b9e599f9a45346dcac103988d662bb7f941866
prosourav <sourav19961010@gmail.com>
2021-05-29 23:34:01 +0530

    changed backend code fully

--- index.js
+++ index.js
@@ -1,102 +1,99 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 8000 ;
-const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
-
 
 app.use(cors());
 app.use(express.json());
 
+const MongoClient = require('mongodb').MongoClient;
+const { ObjectID, ObjectId } = require('bson');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lahub.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log('error : ',err);
+  const foodCollection = client.db("redonion").collection("dishes");
+  // new
+  const aboutCollection = client.db("redonion").collection("About");
+  // 
+  console.log('successFull');
 
-
-
-const uri = process.env.DB_PATH;
-let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
+// server creation
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/dishes',(req,res)=>{
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        if(err){
-            console.log(err);
-        }else{
-            const collection = client.db('redonion').collection('dishes');
-            collection.find().toArray((rej,documents) => {
-                if(rej){
-                    console.log(rej);
-                    res.status(500).send("Filed to Fetch Data ")
-                }else{
-                    res.send(documents);
-                }
-                // client.close();
-            })
-        }
+//Adding new books to shop
+
+  app.post('/adddish', (req, res)=>{
+    const dish = req.body;
+
+    foodCollection.insertOne(dish)
+    .then(result =>{
+      console.log('inserted count: ',result.insertedCount)
+      res.send(result.insertedCount > 0)
     })
-})
+    
+  })
+  app.post('/about', (req, res)=>{
+    const about = req.body;
 
-app.get('/About',(req,res)=>{
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        if(err){
-            console.log(err);
-        }else{
-      
-            const collection =  client.db('redonion').collection('About');
-            collection.find().toArray((rej,documents) => {
-                if(rej){
-                    console.log(rej);
-                    res.status(500).send("Filed to Fetch Data ")
-                }else{
-                    res.send(documents);
-                }
-                // client.close();
-            })
-        }
+     aboutCollection.insertOne(about)
+    .then(result =>{
+      console.log('inserted count: ',result.insertedCount)
+      res.send(result.insertedCount > 0)
     })
-})
+    
+  })
 
-app.post('/adddish',(req,res)=>{
-    const Newfood = req.body;
-    console.log(Newfood);
-    client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
-    client.connect(err=>{
-        const collection = client.db('redonion').collection('dishes');
-        collection.insertOne(Newfood , (rej, result) =>  {
-        if(rej){
-            res.status(500).send("Filed to insert")
-        }else{
-            res.send(result.insertedId);
-        }
-
-        })
+  app.get('/dishes',(req, res)=>{
+    foodCollection.find()
+    .toArray((err, items)=>{
+      res.send(items);
     })
-})
+  })
+  app.get('/about',(req, res)=>{
+    aboutCollection.find()
+    .toArray((err, items)=>{
+      res.send(items);
+    })
+  })
+// check out data
+  app.get('/fooddetail/:id',(req, res)=>{
+    const id = req.params.id;
+    foodCollection.findOne({_id:ObjectId(req.params.id)})
+    .then(result=>{
+      res.send(result);
+    })
+  })
+ 
 
-app.post('/addAbout',(req,res)=>{
-    const About = req.body;
-    console.log(About);
-    client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
-    client.connect(err=>{
-        const collection = client.db('redonion').collection('About');
-        collection.insertOne(About, (rej, result) =>  {
-        if(rej){
-            res.status(500).send("Filed to insert")
-        }else{
-            res.send(result.insertedId);
-        }
+//   //order data 
+//   app.post('/order',(req,res)=>{
+//     const newOrder = req.body;
+//     console.log("New order: ",newOrder);
+//     ordersCollection.insertOne(newOrder)
+//     .then(result =>{
+//       console.log('inserted count: ',result.insertedCount)
+//       res.send(result.insertedCount > 0);
+//     })
+//   })
 
-        })
-    }
-    )
-})
+//   app.get('/order',(req,res)=>{
+//     ordersCollection.find({email:req.query.email})
+//     .toArray((err, items)=>{
+//       res.send(items);
+//     })
+//   })
+
+
+
+
+
+//   client.close();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
